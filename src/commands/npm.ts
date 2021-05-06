03e73f5d97752bdc732f7989c09037ef926d8310
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-05-06 10:09:11 -0400

    finish npm command

--- src/commands/npm.ts
+++ src/commands/npm.ts
@@ -1,28 +1,66 @@
 import { Command } from "@aeroware/aeroclient/dist/types";
 import { MessageEmbed } from "discord.js";
 import fetch from "node-fetch";
+import { utils } from "@aeroware/aeroclient";
+import { oneLine } from "common-tags";
 
 export default {
     name: "npm",
-    aliases: [],
     category: "information",
     description: "Searches npm for packages.",
-    details: "Provide a query or name of a package.",
+    details:
+        "Provide a query or name of a package. Wrap package name in `backticks` to search a package under a user (like `@aeroware/aeroclient`).",
     usage: "<query>",
     minArgs: 1,
     async callback({ message, args }) {
-        const query = args.join(" ");
+        let query = args.join(" ");
 
-        const json = await (await fetch(`https://api.npms.io/v2/search?q=${encodeURIComponent(query)}`)).json();
+        if (query.startsWith("`") && query.endsWith("`"))
+            query = query.slice(1, query.length - 2);
+
+        const json = await (
+            await fetch(
+                `https://api.npms.io/v2/search?q=${encodeURIComponent(query)}`
+            )
+        ).json();
 
         const first = json.results[0];
 
-        console.log(first);
+        if (json.results.length === 1) {
+            const sent = await message.channel.send(
+                new MessageEmbed()
+                    .setTitle(first.package.name)
+                    .setURL(first.package.links.npm)
+                    .setDescription(first.package.description)
+                    .setFooter(`v${first.package.version}`)
+            );
 
-        message.channel.send(
-            json.total === 1
-                ? new MessageEmbed().setTitle(first.package.name).setURL(first.package.links.npm).setDescription(first.package.description).setFooter(`version ${first.package.version}`)
-                : new MessageEmbed()
-        );
+            sent.delete({ timeout: 60000 });
+            return;
+        } else {
+            utils.paginate(
+                message,
+                json.results
+                    .reverse()
+                    .slice(-10)
+                    .reverse()
+                    .map((r: any) =>
+                        new MessageEmbed()
+                            .setTitle(r.package.name)
+                            .setURL(r.package.links.npm)
+                            .setDescription(r.package.description)
+                            .setFooter(`v${r.package.version}`)
+                    ),
+                {
+                    time: 60000,
+                    fastForwardAndRewind: {
+                        time: 5000,
+                    },
+                    goTo: {
+                        time: 5000,
+                    },
+                }
+            );
+        }
     },
 } as Command;
